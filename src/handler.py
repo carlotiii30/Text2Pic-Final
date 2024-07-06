abfe5965839f72f394ad152c75312a98b2be4c71
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2024-07-06 08:49:48 +0200

    Added handler. Closes #24

---
+++ src/handler.py
@@ -0,0 +1,88 @@
+import json
+import logging
+
+
+# pylint: disable=too-few-public-methods
+class Handler:
+    def __init__(self, socket):
+        self.socket = socket
+
+    def handle(self):
+        with self.socket:
+            data = self._receive_data()
+            request = self._process_request(data)
+            response = self._execute_command(request)
+            self._send_response(response)
+
+    def _receive_data(self):
+        return self.socket.recv(1024).decode()
+
+    def _process_request(self, data):
+        try:
+            return json.loads(data)
+
+        except json.JSONDecodeError as e:
+            response = {
+                "status": "error",
+                "message": f"Error decoding JSON: {str(e)}",
+            }
+
+            logging.error("Error decoding JSON: %s", str(e))
+
+            return response
+
+    def _execute_command(self, request):
+        command = request.get("command")
+        text = request.get("text", "")
+
+        if command == "process_text":
+            response = self._process_text(text)
+
+        if command == "generate_number":
+            response = self._generate_number(text)
+
+        elif command == "generate_image":
+            response = self._generate_image(text)
+
+        else:
+            response = {
+                "status": "error",
+                "message": f"Unknown command: {command}",
+            }
+
+            logging.error("Unknown command: %s", command)
+
+        return response
+
+    def _send_response(self, response):
+        self.socket.sendall(json.dumps(response).encode())
+
+    def _process_text(self, text):
+        response = {
+            "status": "error",
+            "message": "Command not implemented",
+        }
+
+        logging.error("Command not implemented: %s", text)
+
+        return response
+
+    def _generate_number(self, text):
+        response = {
+            "status": "error",
+            "message": "Command not implemented",
+        }
+
+        logging.error("Command not implemented: %s", text)
+
+        return response
+
+    def _generate_image(self, text):
+        response = {
+            "status": "error",
+            "message": "Command not implemented",
+        }
+
+        logging.error("Command not implemented: %s", text)
+
+        return response
