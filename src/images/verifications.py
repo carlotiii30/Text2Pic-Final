fab76c048ddd124baa0389d8572c7bf0703d9ef3
Carlota de la Vega <carlotadlavega@correo.ugr.es>
2024-07-07 12:10:59 +0200

    Preprocessed text verified. Closes #37

--- src/images/verifications.py
+++ src/images/verifications.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from pycocotools.coco import COCO
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 from src.images.dataset import preprocess_image
+
+max_length = 20
 
 
 def verify_image_preprocessing(data_dir, annotation_file, num_samples=5):
@@ -26,7 +30,32 @@ def verify_image_preprocessing(data_dir, annotation_file, num_samples=5):
         plt.show()
 
 
+def verify_text_preprocessing(data_dir, annotation_file, tokenizer_path='data/tokenizer.pkl', num_samples=5):
+    coco = COCO(annotation_file)
+    image_ids = coco.getImgIds()
+    captions = []
+
+    for img_id in image_ids[:num_samples]:
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        anns = coco.loadAnns(ann_ids)
+        for ann in anns:
+            if "caption" in ann:
+                captions.append(ann["caption"])
+
+    with open(tokenizer_path, 'rb') as file:
+        tokenizer = pickle.load(file)
+
+    sequences = tokenizer.texts_to_sequences(captions)
+    padded_sequences = pad_sequences(sequences, maxlen=max_length)
+
+    for original, sequence, padded in zip(captions, sequences, padded_sequences):
+        print(f"Original: {original}")
+        print(f"Sequence: {sequence}")
+        print(f"Padded: {padded}")
+        print()
+
 data_dir = "data/coco"
 annotation_file = f"{data_dir}/annotations/captions_train2017.json"
 images_dir = f"{data_dir}/train2017"
 verify_image_preprocessing(images_dir, annotation_file)
+verify_text_preprocessing(data_dir, annotation_file)
