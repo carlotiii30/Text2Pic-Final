c613326cc5a0f751463582e6b86d4f6fb4a7721d
Carlota de la Vega <carlota@MacBook-Pro-de-Carlota.local>
2024-08-14 18:42:40 +0200

    Using full dataset

--- src/images/run.py
+++ src/images/run.py
@@ -22,39 +22,41 @@ def generate_and_visualize_image(generator, text_sequence):
 data_dir = "data/coco"
 annotation_file = os.path.join(data_dir, "annotations/captions_train2017.json")
 train_dir = os.path.join(data_dir, "train2017")
-subset = dataset.load_coco_subset(train_dir, annotation_file)
+# subset = dataset.load_coco_subset(train_dir, annotation_file)
+full_dataset = dataset.load_coco_dataset(train_dir, annotation_file)
 
 # Construir modelos
 generator, discriminator = builders.build_models()
 combined = builders.build_conditional_gan(generator, discriminator)
 
 # # Entrenar modelos
-# training.train(generator, discriminator, combined, subset)
+training.train(generator, discriminator, combined, full_dataset)
 
 # # Guardar los modelos
-# utils.save_model_weights(generator, "data/models/generator_weights.weights.h5")
-# utils.save_model_weights(discriminator, "data/models/discriminator_weights.weights.h5")
-# utils.save_model_weights(combined, "data/models/combined_weights.weights.h5")
+utils.save_model_weights(generator, "data/models/generator_weights.weights.h5")
+utils.save_model_weights(discriminator, "data/models/discriminator_weights.weights.h5")
+utils.save_model_weights(combined, "data/models/combined_weights.weights.h5")
 
-generator = utils.load_model_with_weights(
-    generator, "data/models/generator_weights.weights.h5"
-)
-discriminator = utils.load_model_with_weights(
-    discriminator, "data/models/discriminator_weights.weights.h5"
-)
-combined = utils.load_model_with_weights(
-    combined, "data/models/combined_weights.weights.h5"
-)
+# Cargar los modelos
+# generator = utils.load_model_with_weights(
+#     generator, "data/models/generator_weights.weights.h5"
+# )
+# discriminator = utils.load_model_with_weights(
+#     discriminator, "data/models/discriminator_weights.weights.h5"
+# )
+# combined = utils.load_model_with_weights(
+#     combined, "data/models/combined_weights.weights.h5"
+# )
 
 # Tokenizer
-tokenizer_path = "data/tokenizer.pkl"
+# tokenizer_path = "data/tokenizer.pkl"
 
-with open(tokenizer_path, "rb") as file:
-    tokenizer = pickle.load(file)
+# with open(tokenizer_path, "rb") as file:
+#     tokenizer = pickle.load(file)
 
-# Preprocesar el texto de entrada
-input_text = "A giraffe"
-text_sequence = dataset.preprocess_text(input_text, tokenizer, text_process.max_length)
+# # Preprocesar el texto de entrada
+# input_text = "A giraffe"
+# text_sequence = dataset.preprocess_text(input_text, tokenizer, text_process.max_length)
 
-# Generar y visualizar la imagen
-generate_and_visualize_image(generator, text_sequence)
+# # Generar y visualizar la imagen
+# generate_and_visualize_image(generator, text_sequence)
